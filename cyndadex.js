a36b98e59925e9c4b675aed82041183dc7fd991b
Juliana Caldani <julicaldani@gmail.com>
2024-02-28 19:04:05 -0300

    development

--- cyndadex.js
+++ cyndadex.js
@@ -1,4 +1,4 @@
-/*  const backButton = document.querySelector('.backButton'); //// ????
+/*  const backButton = document.querySelector('.backButton'); 
 
   function switchToPage(pageUrl) {
       window.location.href = pageUrl;
@@ -13,3 +13,31 @@ const backButton = document.querySelector('.backButton');
 backButton.addEventListener('click', () => {
     window.location.href = 'index.html';
 });
+
+// modal
+
+// Get the modal
+var modal = document.getElementById("pokeModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("modalBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
