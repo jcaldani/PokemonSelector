e0f74f29751074181b518a4ee6e2f77d24c06cd5
Juliana Caldani <julicaldani@gmail.com>
2024-02-28 21:10:02 -0300

    development

--- totodex.js
+++ totodex.js
@@ -2,3 +2,24 @@ const backButton = document.querySelector('.backButton');
 backButton.addEventListener('click', () => {
     window.location.href = 'index.html';
 });
+
+var modal = document.getElementById("pokeModal");
+
+var btn = document.getElementById("modalBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
+
